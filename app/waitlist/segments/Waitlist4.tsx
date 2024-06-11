624f3fb851f3b9fcd1e1ed6c11bd7854a9528a4c
Tanishq Bedi <tanishq.bedi@gmail.com>
2024-06-11 21:51:17 +0530

    bug fixes

--- app/waitlist/segments/Waitlist4.tsx
+++ app/waitlist/segments/Waitlist4.tsx
@@ -12,7 +12,7 @@ export function Waitlist4() {
         contentRefs.current.forEach((ref, index) => {
             if (ref) {
                 const contentHeight = ref.scrollHeight;
-                ref.style.height = expandedIndex === index ? `auto` : '50px';
+                ref.style.height = expandedIndex === index ? `100%` : '50px';
             }
         });
     }, [expandedIndex]);
@@ -26,17 +26,15 @@ export function Waitlist4() {
             <div className="flex flex-col h-full w-full items-center justify-center text-white transition-all transform">
                 <h1 className="text-[42px] font-[700]">FA<span className="text-[#6FFFE9]">Q</span></h1>
                 <div className="w-[60%] flex flex-col gap-10">
-                    {['Who is the CEO of Semi Hyphen?', 'What are the features and options on Semi Hyphen?', 'This year growth in Semi Hyphen?'].map((question, index) => (
+                    {['Who is the CEO of Semi Hyphen?', 'What is the vision of Semi Hyphen?', 'How can I join Semi Hyphen?'].map((question, index) => (
                         <div
                             key={index}
-                            ref={(el) => contentRefs.current[index] = el}
+                            ref={el => { contentRefs.current[index] = el; }}
                             className={`flex items-center transition-height duration-500 gap-5 ${expandedIndex === index ? 'bg-gradient-to-bg' : 'bg-[#040404]'} border border-[#3A506B] p-[10px] rounded-[10px] text-[24px]`}
                             onClick={() => toggleExpand(index)}
                         >
                             <Image src={faq} alt="" className={`${expandedIndex === index ? 'rotate-45' : ''} transition-all duration-500`} />
-                            {
-                                expandedIndex !== index && (<span>{question}</span>)
-                            }
+                            {expandedIndex !== index && (<span>{question}</span>)}
                             <span className={`${expandedIndex === index ? 'block' : 'hidden'}`}>
                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur fugiat hic, voluptate minima vitae sit. Iusto at fugiat repudiandae soluta dignissimos aperiam quia nisi necessitatibus, ullam perspiciatis non facere laborum.
                             </span>
